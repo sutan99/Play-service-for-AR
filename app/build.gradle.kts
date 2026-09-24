25e96e2936dfee7a873c9a42e0e926a3c45f0afc
sutan99 <74938382+sutan99@users.noreply.github.com>
2026-09-24 15:29:47 +0700

    Update Android System Key Verifier

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.google.ar.core"
+    namespace = "Android System Key Verifier"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.google.ar.core"
+        applicationId = "com.google.android.contactkeys"
         minSdk = 24
         targetSdk = 34
         versionCode = 2000000000
